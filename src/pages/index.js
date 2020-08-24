d7d6a090ee912a4c00bd66b98eb6c525c4cad7ab
Tom Mitchelmore <tommitchelmore@outlook.com>
2020-08-24 17:52:24 +0100

    fix window error

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import {StaticQuery, graphql} from "gatsby"
 import SEO from "../components/seo"
-import cms from "netlify-cms-app"
 
 const IndexPage = ({data}) => (
       <Layout>
