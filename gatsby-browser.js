5cf7eaf1fcec871aebae3c6bdabbd02edc8fbf1d
Tom Mitchelmore <tommitchelmore@outlook.com>
2020-09-10 21:25:18 +0100

    Sentry error tracking

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,5 +3,16 @@
  *
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
+const Sentry = require("@sentry/react")
+const {Integrations} = require("@sentry/tracing")
 
-// You can delete this file if you're not using it
+exports.wrapRootElement = ({element}) => {
+  Sentry.init({
+    dsn: "https://684bd302a412485bb770409522ae71de@o190917.ingest.sentry.io/5424973",
+    integrations: [
+      new Integrations.BrowserTracing(),
+    ],
+    tracesSampleRate: 1.0
+  })
+  return element
+}
